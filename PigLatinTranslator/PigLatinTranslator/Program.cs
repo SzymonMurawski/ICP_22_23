57be06b254e09a64bbf59f6953cbe698fd1851a4
Szymon Murawski <szymon.murawski@wsb.poznan.pl>
2022-11-29 13:46:34 +0100

    feat: add pig latin translator

---
+++ PigLatinTranslator/PigLatinTranslator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PigLatinTranslator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in sentence to translate");
+            string sentence = Console.ReadLine();
+            string translatedSentence = TranslateSentence(sentence);
+            Console.WriteLine($"In pig lating: {translatedSentence}");
+        }
+        static string TranslateSingleWord(string word)
+        {
+            string translatedWord = word.Substring(1) + word[0] + "ay";
+            return translatedWord;
+        }
+        static string TranslateSentence(string sentence)
+        {
+            string[] words = sentence.Split(' ');
+            string translatedSentence = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                translatedSentence += $" {TranslateSingleWord(words[i])}";
+            }
+            return translatedSentence;
+        }
+    }
+}
