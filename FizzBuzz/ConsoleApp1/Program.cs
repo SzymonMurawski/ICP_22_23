e43b40c359d9f3a68f294296e68bb00423e4a781
szymon.murawski <szymon.murawski@wsb.poznan.pl>
2022-12-06 13:25:38 +0100

    FizzBuzz

---
+++ FizzBuzz/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz(10);
+
+
+            void FizzBuzz(int n)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        public void FizzBuzz(int n)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+FizzBuzz(10);
+
+
+void FizzBuzz(int n)
+{
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+void FizzBuzzButBetter(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        string output = "";
+
+        if (i % 3 == 0)
+        {
+            output += "Fizz";
+        }
+        
+        if (i % 5 == 0)
+        {
+            output += "Buzz";
+        }
+
+        if(output == "")
+        {
+            output = i.ToString();
+        }
+        Console.WriteLine(output);
+    }
+}
