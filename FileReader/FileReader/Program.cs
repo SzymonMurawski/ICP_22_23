9210d5218573fddfbfdbe6b467e8df2dc9fae782
Szymon Murawski <szymon.murawski@gmail.com>
2022-12-13 22:17:09 +0100

    feat: add FileReader program

---
+++ FileReader/FileReader/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(@"Please provie an absolute path to file (like C:\data.txt)");
+string filePath = Console.ReadLine();
+StreamReader sr = new StreamReader(filePath);
+int[] countOfWordsOfLength = new int[20];
+int totalWordsCount = 0;
+while (!sr.EndOfStream)
+{
+    string line = sr.ReadLine();
+    string[] words = line.Split(' ');
+    for(int i = 0; i < words.Length; i++)
+    {
+        totalWordsCount++;
+        string word = words[i];
+        if (word.Length < countOfWordsOfLength.Length)
+        {
+            countOfWordsOfLength[i] = word.Length;
+        }
+    }
+
+}
+sr.Close();
+Console.WriteLine($"In file {filePath} there are {totalWordsCount} words:");
+for (int i = 1; i < countOfWordsOfLength.Length; i++)
+{
+    Console.WriteLine($"{countOfWordsOfLength[i]} words of length {i},");
+}
