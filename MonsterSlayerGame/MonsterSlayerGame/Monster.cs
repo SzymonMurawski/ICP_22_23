c9f2c2a1c7b6ea8b6e761460d77a6e38b68b2072
Szymon Murawski <szymon.murawski@gmail.com>
2022-12-18 20:30:46 +0100

    feat[MonsterSlayerGame] - extracted Monster class

---
+++ MonsterSlayerGame/MonsterSlayerGame/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterSlayerGame
+{
+    public class Monster
+    {
+        public string Name;
+        public int Health;
+        public string Weapon;
+        public Monster(string name, int health, string weapon)
+        {
+            Name = name;
+            Health = health;
+            Weapon = weapon;
+        }
+    }
+}
--- MonsterSlayerGame/MonsterSlayerGame/Program.cs
+++ MonsterSlayerGame/MonsterSlayerGame/Program.cs
@@ -1,46 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MonsterSlayerGame;
+
 Console.Write("What is your name hero? ");
 string playerName = Console.ReadLine();
 int playerHealth = 100;
 string playerWeapon = "2d6";
 string playerBlock = "1d20";
-
-string monsterName = "Goblin";
-int monsterHealth = 15;
-string monsterWeapon = "1d6";
+Monster monster = new Monster("Goblin", 15, "1d6");
 
 while (true)
 {
-    Console.WriteLine($"{playerName} with {playerHealth}hp fights {monsterName} with {monsterHealth}hp");
+    Console.WriteLine($"{playerName} with {playerHealth}hp fights {monster.Name} with {monster.Health}hp");
     Console.Write($"Given choice between ATTACK and BLOCK {playerName} decides to ");
     string choice = Console.ReadLine();
-    int monsterAttackValue = rollDice(monsterWeapon);
+    int monsterAttackValue = rollDice(monster.Weapon);
     switch (choice)
     {
         case "ATTACK":
             int playerAttackValue = rollDice(playerWeapon);
             playerHealth -= monsterAttackValue;
-            monsterHealth -= playerAttackValue;
-            Console.WriteLine($"{playerName} attacks for {playerAttackValue}, but {monsterName} responds with a hit for {monsterAttackValue}");
+            monster.Health -= playerAttackValue;
+            Console.WriteLine($"{playerName} attacks for {playerAttackValue}, but {monster.Name} responds with a hit for {monsterAttackValue}");
             break;
         case "BLOCK":
             int playerBlockValue = rollDice(playerBlock);
             // this is a bug/feature - if block value is higher than attack value, player will be healed
             playerHealth -= monsterAttackValue - playerBlockValue;
-            Console.WriteLine($"{monsterName} tries to hit for {monsterAttackValue}, but {playerName} blocks {playerBlockValue}");
+            Console.WriteLine($"{monster.Name} tries to hit for {monsterAttackValue}, but {playerName} blocks {playerBlockValue}");
             break;
         default:
             playerHealth -= monsterAttackValue;
-            Console.WriteLine($"While {playerName} tries in vain to {choice}, {monsterName} takes a swing for {monsterAttackValue}");
+            Console.WriteLine($"While {playerName} tries in vain to {choice}, {monster.Name} takes a swing for {monsterAttackValue}");
             break;
     }
-    if (monsterHealth <= 0)
+    if (monster.Health <= 0)
     {
-        Console.WriteLine($"{playerName} defeated {monsterName}!");
+        Console.WriteLine($"{playerName} defeated {monster.Name}!");
         break;
     } else if (playerHealth <= 0)
     {
-        Console.WriteLine($"{playerName} was vanquished by {monsterName}");
+        Console.WriteLine($"{playerName} was vanquished by {monster.Name}");
         break;
     }
 }
