e5589cd80bb0b3c1bee0547ef3b91254f2856fb3
Szymon Murawski <szymon.murawski@gmail.com>
2023-01-07 23:06:53 +0100

    feat: JokeApp

---
+++ JokeApp/JokeApp/JokeService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace JokeApp
+{
+    public class JokeService
+    {
+        public static Joke GetRandomJoke()
+        {
+            return DoGetJoke("Any");
+        }
+        private static Joke DoGetJoke(string category)
+        {
+            category ??= "Any";
+            using HttpClient client = new();
+            String json = client.GetStringAsync($"https://v2.jokeapi.dev/joke/{category}?blacklistFlags=nsfw,racist,sexist&type=twopart").Result;
+            Joke joke = JsonSerializer.Deserialize<Joke>(json);
+            return joke;
+        }
+
+    }
+}
